8979b529d1f15f41310546a5735169bfa043adf4
kanika1930 <kanikaggg5@gmail.com>
2025-07-03 18:46:11 +0530

    Update linux-ssh.sh

--- linux-ssh.sh
+++ linux-ssh.sh
@@ -6,7 +6,7 @@ set -e
 
 # Predefined environment variables
 LINUX_USER_PASSWORD="krish"
-NGROK_AUTH_TOKEN="2zJcs8kKjS32Sr6JayrvHoOspo8_7gRWaJKxyzs3D1T3RfFQn"
+NGROK_AUTH_TOKEN="2zMbWk0BwmMEKuXaxYemxLxyVDN_6hrjoNum3Zj81z2usQeWC"
 LINUX_USERNAME="krish"
 LINUX_MACHINE_NAME="krish"
 
